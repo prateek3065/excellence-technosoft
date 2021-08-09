dfc7fda0711d3aaa0629890af16ff6f3c41e1316
prateek3065 <root@PRATEEKs-MacBook-Pro.local>
2021-08-09 18:09:41 +0530

    added button and text area

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import { useRef, useEffect, useState } from "react";
 export default function ToDoList() {
+  const enteredText = useRef("");
+  const [enteredTodo, setEnteredTodo] = useState(() => {
+    return null;
+  });
+  var handleKeyPress = (event) => {
+    if (event.key === "Enter") setEnteredTodo(enteredText.current.value);
+  };
+  useEffect(() => {
+    console.log("enteredTodo changed="+enteredTodo);
+  }, [enteredTodo]);
   return (
     <div className="to-do-list">
-      <h1>inside to do list</h1>
+      <h1>Please Enter ToDo here</h1>
+      <input
+        type="text"
+        placeholder="   Please Enter"
+        className="input-to-do"
+        ref={enteredText}
+        onKeyPress={handleKeyPress}
+      ></input>
+      <button className="enter-button">Add</button>
     </div>
   );
 }
