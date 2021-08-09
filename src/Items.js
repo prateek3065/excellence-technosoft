e5d11d46b77b50e2a5726b556288130465a8ca74
prateek3065 <root@PRATEEKs-MacBook-Pro.local>
2021-08-09 22:30:14 +0530

    used local storage

---
+++ src/Items.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Items(storedTodo) {
+  console.log("Inside items" + storedTodo);
+  return <div></div>;
+}
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,27 +1,56 @@
 import React from "react";
 import { useRef, useEffect, useState } from "react";
+import Items from "./Items";
+const { v4: uuidv4 } = require("uuid");
 export default function ToDoList() {
   const enteredText = useRef("");
-  const [enteredTodo, setEnteredTodo] = useState(() => {
-    return null;
+  const [storedTodo, setStoredTodo] = useState(() => {
+    const storedList = JSON.parse(localStorage.getItem("todo"));
+    if (storedList) return storedList;
+    return [];
   });
+
   var handleKeyPress = (event) => {
-    if (event.key === "Enter") setEnteredTodo(enteredText.current.value);
+    if (event.key === "Enter") {
+      //setEnteredTodo((prev)=>enteredText.current.value);
+
+      addTodo();
+      enteredText.current.value = "";
+    }
   };
+
+  var addTodo = () => {
+    const name = enteredText.current.value;
+    if (name === "") return;
+
+    //console.log("i am inside addTodo");
+
+    setStoredTodo((prev) => {
+      return [...prev, { id: uuidv4(), name: name, complete: false }];
+    });
+  };
+
   useEffect(() => {
-    console.log("enteredTodo changed="+enteredTodo);
-  }, [enteredTodo]);
+    localStorage.setItem("todo", JSON.stringify(storedTodo));
+    //console.log(storedTodo);
+  }, [storedTodo]);
+  console.log(storedTodo);
   return (
-    <div className="to-do-list">
-      <h1>Please Enter ToDo here</h1>
-      <input
-        type="text"
-        placeholder="   Please Enter"
-        className="input-to-do"
-        ref={enteredText}
-        onKeyPress={handleKeyPress}
-      ></input>
-      <button className="enter-button">Add</button>
+    <div>
+      <div className="to-do-list">
+        <h1>Please Enter ToDo here</h1>
+        <input
+          type="text"
+          placeholder="   Please Enter"
+          className="input-to-do"
+          ref={enteredText}
+          onKeyPress={handleKeyPress}
+        ></input>
+        <button className="enter-button">Add</button>
+      </div>
+      <div className="items-added">
+        <Items storedTodo={storedTodo} />
+      </div>
     </div>
   );
 }
